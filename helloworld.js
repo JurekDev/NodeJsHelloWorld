9b432c4f0764c3eb80b6d85730c5e1f1bb895a6d
JurekDev <jurek.mueller@outlook.de>
2018-10-01 21:18:18 +0200

    Changed srting
    Changed srting

--- helloworld.js
+++ helloworld.js
@@ -3,7 +3,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-rl.question("Hi!\nWrite something:\n", (input) => {
+rl.question("Hi!\nWrite Hello World:\n", (input) => {
   if (input === "Hello World") {
     console.log("You typed Hello World");
     rl.close();
