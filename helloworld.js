4bc5ef4dd702bb364fb7e28145294a2a6e5cd261
Wilko Müller <wimueller@uos.de>
2018-10-01 21:09:53 +0200

    Added initial hello world in nodejs. Pull requests are welcomed.

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question("Hi!\nWrite something:\n", (input) => {
+  if (input === "Hello World") {
+    console.log("You typed Hello World");
+    rl.close();
+  }
+  else {
+    console.log("You did not type Hello World. Try again!\n");
+  }
+});
